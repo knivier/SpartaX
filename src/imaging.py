0526bfefe5c9d1e3c2b1687392feadcf82c6f26a
Ardusa <ardusa05@gmail.com>
2025-02-01 19:49:16 -0500

    formatting stuff

--- src/imaging.py
+++ src/imaging.py
@@ -5,8 +5,6 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 from mediapipe.framework.formats import landmark_pb2
 
-pygame.init()
-
 WIDTH, HEIGHT = 1920, 1080
 """Width and height of the Pygame screen"""
 MIN_DETECTION_CONFIDENCE = 0.8
@@ -20,11 +18,45 @@ NUM_POSES = 2
 MODEL_PATH = "pose_landmarker_full.task"
 """Path to the pose landmarker model"""
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
+pygame.init()
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+to_window = None
+last_timestamp_ms = 0
+
+
+def print_result(
+    detection_result: vision.PoseLandmarkerResult,  # type: ignore
+    output_image: mp.Image,
+    timestamp_ms: int,
+):
+    global to_window
+    global last_timestamp_ms
+    if timestamp_ms < last_timestamp_ms:
+        return
+    last_timestamp_ms = timestamp_ms
+    to_window = cv2.cvtColor(
+        draw_landmarks_on_image(output_image.numpy_view(), detection_result),
+        cv2.COLOR_RGB2BGR,
+    )
+
+
+def define_action():
+    pass
+
+options = vision.PoseLandmarkerOptions(
+    base_options=python.BaseOptions(model_asset_path=MODEL_PATH),
+    running_mode=vision.RunningMode.LIVE_STREAM,
+    num_poses=NUM_POSES,
+    min_pose_detection_confidence=MIN_DETECTION_CONFIDENCE,
+    min_pose_presence_confidence=MIN_PRESENCE_CONFIDENCE,
+    min_tracking_confidence=MIN_TRACKING_CONFIDENCE,
+    output_segmentation_masks=False,
+    result_callback=print_result,
+)
 
 
 def draw_landmarks_on_image(rgb_image, detection_result):
@@ -56,7 +88,6 @@ def draw_landmarks_on_image(rgb_image, detection_result):
         #     print("Pose landmarks[0]:", detection_result.pose_landmarks[0])
 
         if detection_result.pose_landmarks and True:  # Disable for now
-            # pose_landmarks = detection_result.pose_landmarks[0]
             left_wrist = pose_landmarks[mp.solutions.pose.PoseLandmark.LEFT_WRIST]
             right_wrist = pose_landmarks[mp.solutions.pose.PoseLandmark.RIGHT_WRIST]
 
@@ -93,61 +124,22 @@ def draw_landmarks_on_image(rgb_image, detection_result):
     return annotated_image
 
 
-to_window = None
-last_timestamp_ms = 0
-
-
-def print_result(
-    detection_result: vision.PoseLandmarkerResult,  # type: ignore
-    output_image: mp.Image,
-    timestamp_ms: int,
-):
-    global to_window
-    global last_timestamp_ms
-    if timestamp_ms < last_timestamp_ms:
-        return
-    last_timestamp_ms = timestamp_ms
-    to_window = cv2.cvtColor(
-        draw_landmarks_on_image(output_image.numpy_view(), detection_result),
-        cv2.COLOR_RGB2BGR,
-    )
-
-
-base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
-options = vision.PoseLandmarkerOptions(
-    base_options=base_options,
-    running_mode=vision.RunningMode.LIVE_STREAM,
-    num_poses=NUM_POSES,
-    min_pose_detection_confidence=MIN_DETECTION_CONFIDENCE,
-    min_pose_presence_confidence=MIN_PRESENCE_CONFIDENCE,
-    min_tracking_confidence=MIN_TRACKING_CONFIDENCE,
-    output_segmentation_masks=False,
-    result_callback=print_result,
-)
-
-def scan():
+def scan() -> any:
     with vision.PoseLandmarker.create_from_options(options) as landmarker:
-        # Main loop to display the stream
         running = True
         while running:
-            # Check for Pygame events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-            # Capture frame from the camera
             ret, frame = cap.read()
             if not ret:
                 break
 
-            # Flip and convert the frame to RGB
             rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-            # Convert the frame to a MediaPipe Image object
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
             timestamp_ms = int(cv2.getTickCount() / cv2.getTickFrequency() * 1000)
-
-            # Perform pose detection asynchronously
+            
             landmarker.detect_async(mp_image, timestamp_ms)
 
             if to_window is not None:
@@ -170,4 +162,4 @@ def scan():
         cap.release()
         pygame.quit()
         
-scan()
+    return define_action()
